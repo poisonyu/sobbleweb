ae1bb64f6b9ceb27e91f32d503c069a63f73dd0a
poisonyu <xuzhouyi433@outlook.com>
2024-08-11 22:12:30 +0800

    2024/8/11

--- upload/db.go
+++ upload/db.go
@@ -1,7 +1,38 @@
 package upload
 
-import "github.com/cyansobble/global"
+import (
+	"github.com/cyansobble/global"
+)
 
 func CreateFileInfo(f *FileInfo) error {
 	return global.DB.Create(f).Error
+}
+
+func QueryFileList(pageInfo PageInfo) ([]FileInfo, int64, error) {
+	var files []FileInfo
+	limit := pageInfo.PageSize
+	offset := limit * (pageInfo.Page - 1)
+	keyWord := pageInfo.KeyWord
+	db := global.DB.Model(&PageInfo{})
+	if len(keyWord) > 0 {
+		db = db.Where("filename like ?", "%"+keyWord+"%")
+	}
+	var total int64
+	if err := db.Count(&total).Error; err != nil {
+		return files, total, err
+	}
+
+	err := db.Limit(limit).Offset(offset).Find(&files).Error
+	return files, total, err
+}
+
+func GetFileByID(id uint) (FileInfo, error) {
+	var fileInfo FileInfo
+	err := global.DB.First(&fileInfo, id).Error
+	return fileInfo, err
+
+}
+
+func DeleteFileByID(id uint) error {
+	return global.DB.Delete(&FileInfo{}, id).Error
 }
--- upload/model.go
+++ upload/model.go
@@ -4,8 +4,14 @@ import "gorm.io/gorm"
 
 type FileInfo struct {
 	gorm.Model
-	FileName string `gorm:"column:filename"`
-	FilePath string `gorm:"column:filepath"`
-	Tag      string `gorm:"column:tag"`
-	Owner    string `gorm:"column:owner"`
+	FileName string `gorm:"column:filename" json:"filename"`
+	FilePath string `gorm:"column:filepath" json:"filepath"`
+	Tag      string `gorm:"column:tag" json:"tag"`
+	Owner    string `gorm:"column:owner" json:"owner"`
+}
+
+type PageInfo struct {
+	PageSize int    `json:"pagesize"`
+	Page     int    `json:"page"`
+	KeyWord  string `json:"keyword"`
 }
