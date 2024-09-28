ac79175bbef613efa4eca2e52cd07c54fb081c4e
poisonyu <xuzhouyi433@outlook.com>
2024-09-28 14:54:57 +0800

    2024/9/28

--- study/designPattern.go
+++ study/designPattern.go
@@ -114,7 +114,11 @@ func T(c *gin.Context) {
 	c.Writer.Header().Del("key")
 
 	strings.HasPrefix(c.Request.RequestURI, "/static/")
+
+	strings.ParseInt("")
+	strings.Atoi("dog")
 	path := c.Request.URL.Path
+
 }
 
 func L() {
